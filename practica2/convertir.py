7a112fe94311665653a78b0222c5fc875cbc0fd9
anhi1 <anhiemolina@gmail.com>
2024-07-05 11:59:53 +0200

    2 forma de hacerlo

--- practica2/convertir.py
+++ practica2/convertir.py
@@ -1,19 +1,40 @@
-# convertir de numero romano a decimal
+# 1 convertir de numero romano a decimal
+
+# def romano_a_decimal(romano:str) -> int:
+#     valores = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+#     suma_total = 0
+#     valor_anterior = 0
+
+#     for simbolo in romano:
+#         valor_actual = valores[simbolo]
+#         if valor_actual > valor_anterior:
+#             suma_total += valor_actual - 2 * valor_anterior
+#         else:
+#             suma_total += valor_actual
+#         valor_anterior = valor_actual
+#     return suma_total 
+
+# numero_romano = input("introduce un numero romano: ")
+# numero_decimal = romano_a_decimal(numero_romano)
+# print(f"el numero decimal es: {numero_decimal}")
+
+
+# 2 convertir de numero romano a decimal
 
 def romano_a_decimal(romano:str) -> int:
     valores = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
-    suma_total = 0
-    valor_anterior = 0
+    total = 0
+    previo = 0
 
-    for simbolo in romano:
-        valor_actual = valores[simbolo]
-        if valor_actual > valor_anterior:
-            suma_total += valor_actual - 2 * valor_anterior
-        else:
-            suma_total += valor_actual
-        valor_anterior = valor_actual
-    return suma_total 
-
-numero_romano = input("introduce un numero romano: ")
+    for simbolo in reversed(romano): 
+        valor = valores[simbolo]
+        if valor < previo:
+            total -= valor 
+        else: 
+            total += valor
+        previo = valor
+    return total
+    
+numero_romano = input("Introduce un número romano: ")
 numero_decimal = romano_a_decimal(numero_romano)
-print(f"el numero decimal es: {numero_decimal}")
+print(f"El número decimal de '{numero_romano}' es: {numero_decimal}")
