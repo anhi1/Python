8b8b0e370335652461ad77cc73d230fcad49ad47
anhi1 <anhiemolina@gmail.com>
2024-07-05 12:39:24 +0200

    1 y 2 ejercicio

--- Examen/ejercicio2.py
+++ Examen/ejercicio2.py
@@ -1,1 +1,15 @@
 # Haz una función para separar una frase en una lista de palabras
+
+def separar_una_frase(frase:str) -> list:
+    palabras = frase.split()
+    return palabras
+
+una_frase = input("dime una frase: ")
+lista_palabras = separar_una_frase(una_frase)
+print("lista de palabras", lista_palabras)
+
+
+
+
+
+
